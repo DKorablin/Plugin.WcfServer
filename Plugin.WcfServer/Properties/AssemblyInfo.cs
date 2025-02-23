157744ff8c6e5b549a5026bb17e9ae6b79ba09d8
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 18:06:45 +0300

    Unify plugins deployment

--- Plugin.WcfServer/Properties/AssemblyInfo.cs
+++ Plugin.WcfServer/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("026c98e3-6331-4f4c-afde-d449845801ac")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/project/Default.aspx?File=122")]
 #else
 
-[assembly: AssemblyTitle("Plugin.WCF Server")]
 [assembly: AssemblyDescription("WCF server for remote plugin calling")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.WCF Server")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2019-2024")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2019-2025")]
 #endif
